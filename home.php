6ab33002063166d8578dff01167c5f9e0f2dc1fb
Maximilian Maske <maximilian.maske@gmail.com>
2017-11-21 22:13:20 +0100

    youtube to vimeo

--- home.php
+++ home.php
@@ -7,20 +7,20 @@ Template Name: Startseite
 <?php get_header();?>
 
 <div id="video-container" class="container">
-<div class="embed-responsive embed-responsive-16by9">
-<iframe class="embed-responsive-item" width="560" height="315" src="https://www.youtube.com/embed/ixmb148iLKI?rel=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>
-</div>
+  <div class="embed-responsive embed-responsive-16by9">
+    <iframe src="https://player.vimeo.com/video/243924953?color=ffffff&title=0&byline=0&portrait=0" width="640" height="360" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+  </div>
 </div>
 
 <div class="strip-container container">
-<?php if ( have_posts() ) : ?>
+  <?php if ( have_posts() ) : ?>
   <?php while ( have_posts() ) : the_post(); ?>
-    
-    <?php the_content();?>
-     
+
+  <?php the_content();?>
+
   <?php endwhile; ?>
   <?php endif; ?>
-  
+
 
 </div><!-- /.container -->
 
