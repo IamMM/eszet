76eaeeab63598b016f4cbf0570c1fa1796b84597
Maximilian Maske <maximilian.maske@gmail.com>
2017-11-21 18:33:01 +0100

    add controls to video

--- home.php
+++ home.php
@@ -8,7 +8,7 @@ Template Name: Startseite
 
 <div id="video-container" class="container">
 <div class="embed-responsive embed-responsive-16by9">
-<iframe class="embed-responsive-item" width="560" height="315" src="https://www.youtube.com/embed/ixmb148iLKI?rel=0&amp;controls=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>
+<iframe class="embed-responsive-item" width="560" height="315" src="https://www.youtube.com/embed/ixmb148iLKI?rel=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>
 </div>
 </div>
 
